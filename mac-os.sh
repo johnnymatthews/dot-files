d291dc9e6e843f44a2f1baa2cd04a547539ef774
johnnymatthews <johnnymatthews@protonmail.com>
2021-11-12 11:56:12 -0500

    Adds MacOS install instructions.

--- mac-os.sh
+++ mac-os.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-WORKING_DIR=$(pwd)
-DOT_FILES_DIR="./dot-files"
+# Define Git variables here.
 GIT_EMAIL=""
 GIT_USERNAME=""
 
+# Check if Git variables have been defined. Exit script if not.
+[[ -z "$GIT_EMAIL" ]] && { echo "Error: GIT_EMAIL variable is empty. Exiting script." ; exit 1; }
+[[ -z "$GIT_USERNAME" ]] && { echo "Error: GIT_USERNAME variable is empty. Exiting script." ; exit 1; }
+
+# Install common programs.
 install_programs() {
     echo ">>> [Install programs] Started..."
 
@@ -13,49 +17,59 @@ install_programs() {
     echo ">>> [Install programs] Homebrew install complete."
 
     echo ">>> [Install programs] Installing apps with brew..."
-    brew install pass pass-otp tmux cmus neovim nodejs youtube-dl
+    brew install pass pass-otp tmux cmus neovim nodejs youtube-dl zsh
     brew install --cask hyper
     echo ">>> [Install programs] Install brew apps completed."
 
     echo ">>> [Install programs] Installing NVM..."
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 
-    # Load MVN.
+    # Load NVM.
     export NVM_DIR="$HOME/.nvm"
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
     # Install latest node version.
     nvm install node
-    
+
+    # Configure ZSH
+    chsh -s $(which zsh)
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+    # Install Vim Plug
+    sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+               https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
     echo ">>> [Install programs] Install NVM completed."
 
     echo ">>> [Install programs] Complete."
 }
 
+# Set configurations for programs, and copy of dot files.
 set_configs() {
     echo ">>> [Set configs] Configuration started..."
+
+    echo ">>> [Set configs] Copying config files..."
+    mkdir ~/.config/alacritty && cp dot-files/alacritty.yml ~/.config/alacritty/
+    mkdir ~/.config/nvim && cp dot-files/init.vim ~/.config/nvim/
+    mkdir ~/.config/zsh && cp dot-files/minimal.zsh ~/.config/zsh/
+    mkdir ~/.config/tmux && cp dot-files/tmux.conf ~/.config/tmux/
+    cp dot-files/vimrc ~/.vimrc
+    cp dot-files/zshrc ~/.zshrc
+    cp dot-files/gitconfig ~/.gitconfig
 
     echo ">>> [Set configs] Setting Git variables..."
     git config --global user.email "$GIT_EMAIL"
     git config --global user.name "$GIT_USERNAME"
-    git config --global pager.branch false
-    git config --global push.default current
-
-    echo ">>> [Set configs] Copying config files..."
-    cp "$DOT_FILES_DIR/bashrc" ~/.bashrc
-    cp "$DOT_FILES_DIR/vimrc" ~/.vimrc
-    cp "$DOT_FILES_DIR/tmux.conf" ~/.config/tmux/tmux.conf
-    cp "$DOT_FILES_DIR/init.vim" ~/.config/nvim/init.vim
-    cp "$DOT_FILES_DIR/hyper.js" ~/.hyper.js
 
     echo ">>> [Set configs] Creating Cmus update script..."
-    mkdir -p ~/.config/cmus
-    cp "$DOT_FILES_DIR/cmus-update-script.sh" ~/.config/cmus/update-library.sh
+    mkdir ~/.config/cmus
+    cp dot-files/update-library.sh ~/.config/cmus/update-library.sh
     chmod +x ~/.config/cmus/update-library.sh
     cmus-remote --raw ":bind -f common u shell ~/.config/cmus/update-library.sh"
 
     echo ">>> [Set configs] Configuration complete."
 }
 
+# Run the two main functions from here.
 install_programs
 set_configs
