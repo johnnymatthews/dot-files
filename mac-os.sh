2250bfc3f6d1c7f9d459433a0c19dd60a269c228
johnnymatthews <johnnymatthews@protonmail.com>
2022-04-25 11:21:34 +0200

    Moves stuff around.

--- mac-os.sh
+++ mac-os.sh
@@ -8,20 +8,15 @@ GIT_USERNAME=""
 [[ -z "$GIT_EMAIL" ]] && { echo "Error: GIT_EMAIL variable is empty. Exiting script." ; exit 1; }
 [[ -z "$GIT_USERNAME" ]] && { echo "Error: GIT_USERNAME variable is empty. Exiting script." ; exit 1; }
 
-# Install common programs.
 install_programs() {
-    echo ">>> [Install programs] Started..."
-
-    echo ">>> [Install programs] Installing Homebrew..."
+    # Install Homebrew
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    echo ">>> [Install programs] Homebrew install complete."
 
-    echo ">>> [Install programs] Installing apps with brew..."
+    # Install Brew apps
     brew install pass pass-otp tmux cmus neovim nodejs youtube-dl zsh
     brew install --cask hyper
-    echo ">>> [Install programs] Install brew apps completed."
 
-    echo ">>> [Install programs] Installing NVM..."
+    # Install NVM
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 
     # Load NVM.
@@ -42,16 +37,14 @@ install_programs() {
     sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
                https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-    echo ">>> [Install programs] Install NVM completed."
-
-    echo ">>> [Install programs] Complete."
+    # Install YT-DLP
+    sudo curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o /usr/local/bin/yt-dlp
+    sudo chmod a+rx /usr/local/bin/yt-dlp
 }
 
-# Set configurations for programs, and copy of dot files.
 set_configs() {
-    echo ">>> [Set configs] Configuration started..."
 
-    echo ">>> [Set configs] Copying config files..."
+    # Copy config files to appropriate dirs.
     mkdir ~/.config/alacritty && cp dot-files/alacritty.yml ~/.config/alacritty/
     mkdir ~/.config/nvim && cp dot-files/init.vim ~/.config/nvim/
     mkdir ~/.config/zsh && cp dot-files/minimal.zsh ~/.config/zsh/
@@ -60,17 +53,15 @@ set_configs() {
     cp dot-files/zshrc ~/.zshrc
     cp dot-files/gitconfig ~/.gitconfig
 
-    echo ">>> [Set configs] Setting Git variables..."
+    # Set Git variables
     git config --global user.email "$GIT_EMAIL"
     git config --global user.name "$GIT_USERNAME"
 
-    echo ">>> [Set configs] Creating Cmus update script..."
+    # Create CMUS update script and set it.
     mkdir ~/.config/cmus
     cp dot-files/update-library.sh ~/.config/cmus/update-library.sh
     chmod +x ~/.config/cmus/update-library.sh
     cmus-remote --raw ":bind -f common u shell ~/.config/cmus/update-library.sh"
-
-    echo ">>> [Set configs] Configuration complete."
 }
 
 # Run the two main functions from here.
