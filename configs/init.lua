d762b35072d8bca4af6a599a6c313e10c8346952
johnnymatthews <9611008+johnnymatthews@users.noreply.github.com>
2024-02-16 10:31:22 -0400

    Adds Lua, because that's what we do now."

---
+++ configs/init.lua
@@ -0,0 +1,21 @@
+-- GENERAL OPTIONS
+-- ==============================================================================
+vim.o.syntax = true
+vim.o.errorbells = false
+vim.o.tabstop =  4
+vim.o.softtabstop =  4
+vim.o.shiftwidth =  4
+vim.o.expandtab = true
+vim.o.smartindent = true
+vim.wo.number = true
+vim.wo.relativenumber = true
+vim.o.smartcase = true
+vim.o.swapfile = false
+vim.o.backup = false
+vim.o.undodir = '~/.vim/undodir'
+vim.o.undofile = true
+vim.o.incsearch = true
+vim.wo.cursorline = true
+vim.o.background = 'dark'
+vim.o.mouse = 'a'
+-- ==============================================================================
